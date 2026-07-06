3ff9766fc9f3f2863168fba566fc474e2ba1cb18
unknown <vanshikaguptaaa2005@gmail.com>
2026-07-06 09:18:16 +0530

    Promises

--- JS/promises.js
+++ JS/promises.js
@@ -1,3 +1,4 @@
+/*
 function step1(callback) {
     setTimeout(() => {
         console.log("Step 1 completed");
@@ -36,12 +37,13 @@ step1(() => {
         });
     });
 });
+*/
 
 /**
  * promise is an object that represents the eventual completion (or failure) of an
  *  asynchronous operation and its resulting value. It allows you to write asynchronous code 
  * in a more manageable way, avoiding the "callback hell" problem seen in the previous example.*/
-
+/*
 const promise=new Promise((resolve, reject) => {
     const success = true; 
     if (success) {
@@ -80,3 +82,83 @@ login()
 .then((orders) => {
     console.log(orders);
 });
+*/
+
+function getProducts(){
+    return new Promise((resolve, reject) => {
+        setTimeout(()=>{
+            let success = true;
+            if(success){
+                resolve("Product Loaded");
+            }
+            else{
+                reject("Promise failed");
+                
+            }
+        },3000)
+    })
+}
+
+function getOffers(){
+    return new Promise((resolve, reject) => {
+        setTimeout(()=>{
+            let success = false;
+            if(success){
+                resolve("Offer Loaded");
+            }
+            else{
+                reject("Offer Request Failed");
+                
+            }
+        },2000)
+    })
+}
+
+function getCategories(){
+    return new Promise((resolve, reject) => {
+        setTimeout(()=>{
+            let success = false;
+            if(success){
+                resolve("Categories Loaded");
+            }
+            else{
+                reject("Categories Request Failed");
+                
+            }
+        },5000)
+    })
+}
+
+/*
+| Method                 | Waits For       | If One Rejects      | Result                  |
+| ---------------------- | --------------- | ------------------- | ----------------------- |
+| `Promise.all()`        | All fulfilled   | Rejects immediately | Array of values         |
+| `Promise.allSettled()` | All settled     | Doesn't matter      | Array of status objects |
+| `Promise.race()`       | First settled   | Rejection can win   | First result/error      |
+| `Promise.any()`        | First fulfilled | Ignores rejects     | First success           |
+
+*/
+
+/*
+Promise.all([getProducts(), getOffers(), getCategories()])
+.then((data) => {
+    console.log("Resolved: ",data);
+})
+.catch((err) => {
+    console.log(err);
+    
+})
+*/
+
+
+Promise.allSettled([getProducts(), getOffers(), getCategories()])
+.then((data) => {
+    console.log("Resolved: ",data);
+})
+.catch((err) => {
+    console.log(err);
+    
+})
+.finally(()=>{
+    console.log("Finally");
+})
