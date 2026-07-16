82c65aa52ad0f534dc48d61cf366162fe3130708
unknown <vanshikaguptaaa2005@gmail.com>
2026-07-16 09:36:53 +0530

    Http Method

--- Node/httpServer.js
+++ Node/httpServer.js
@@ -1,11 +1,14 @@
 const http = require('http');
 const server = http.createServer((req, res) => {
     // res.end("Response from server");
-    if(req.url === '/'){
+    if(req.url === '/' && req.method === 'GET'){
         res.end("Home")
     }
     else if(req.url === '/login'){
         res.end("Login response")
+    }
+    else if(req.url === '/' && req.method === 'POST'){
+        res.end("Response for post request")
     }
     else{
         res.end("Not found");
