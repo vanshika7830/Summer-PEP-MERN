81de3042fdfe3a58f97c1b3e2cdbd5d2e13637a7
unknown <vanshikaguptaaa2005@gmail.com>
2026-07-20 09:08:59 +0530

    Path changed

--- React/second-react-project/src/Components/Footer/Footer.jsx
+++
@@ -1,9 +0,0 @@
-import "./Footer.css"
-function Footer(){
-    return (
-        <div className="footer-container">
-            <p>This is Footer Component</p>
-        </div>
-    )
-}
-export default Footer
