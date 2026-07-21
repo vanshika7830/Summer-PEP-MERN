e17e773c9e5c0fca5c6a2cac0bd17f59f520a9e5
unknown <vanshikaguptaaa2005@gmail.com>
2026-07-21 11:09:46 +0530

    routes added

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,50 +1,17 @@
-// const express = require('express')
-import express from 'express'
-import 'dotenv/config'
-import fs from 'fs/promises'
-const port = process.env.PORT;
-const app = express();
-app.use(express.json())
-app.get('/',(req,res)=>{
-    res.json({message: 'Response from server'})
-})
-app.post('/signup',async (req,res) => {
-   try{
-        // const data = req.body;
-        const {firstName, lastName, email, password} = req.body;
-        const userList = JSON.parse(await fs.readFile('data/user.json','utf-8'));
-        if(!firstName || !lastName || !email || !password){
-            res.json({
-                message: "All info are required for signup"
-            });
-            return;
-        }
-        const existingUser = userList.find(u => u.email === email);
-        if(existingUser){
-            res.json({
-                message: "Email already exist"
-            });
-            return;
-        }
-        const newUser = {
-            "id": userList.length + 1,
-            "firstName": firstName,
-            "lastName": lastName,
-            "email": email,
-            "password": password,
-            "role": "student"
-        }
+import express from 'express';
+import 'dotenv/config';
+import authRoutes from './routes/authRoutes.js';
+import courseRoutes from './routes/courseRoutes.js';
 
-        userList.push(newUser);
-        await fs.writeFile('data/user.json',JSON.stringify(userList,null,2));
-        res.json({
-            message: "New user created successfully",
-            data: newUser
-        })
-   }
-   catch(err){
-        console.log(err);
-   }
-   
+const port = process.env.PORT;
+
+const app = express();
+app.use(express.json());
+
+app.use('/auth', authRoutes);
+
+app.use('/courses', courseRoutes);
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
 })
-app.listen(port, ()=> {console.log(`Server Running on port ${port}`)})
